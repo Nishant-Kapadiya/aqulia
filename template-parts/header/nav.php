46aa65f5ae6c0cbe3a1fb3fa9a8f25c6c07477a9
Nishant-Kapadiya <nishantkapadiya8@gmail.com>
2021-11-01 17:28:38 +0530

    update

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -4,7 +4,9 @@
  *
  * @package Aquila
  */
-
+	$menu_class = \Aquila_Theme\Inc\Menus::get_instance();
+	$header_menu_id = $menu_class->get_menu_id( 'aquila-header-menu' );
+	$header_menus = wp_get_nav_menu_items( $header_menu_id );
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
