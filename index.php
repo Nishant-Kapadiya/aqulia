7a580462000a016a0140f15b88c8b8540d99271a
Nishant-Kapadiya <nishantkapadiya8@gmail.com>
2021-11-04 18:50:40 +0530

    update

--- index.php
+++ index.php
@@ -8,11 +8,24 @@
 get_header();
 
 ?>
-
-<div class="content">
-	<?php esc_html_e( 'Content Blog' , 'aquila' ); ?>
+<div id="primary">
+	<main id="main" class="site-main mt-5" role="main">		
+		<?php
+			if ( have_posts() ) : 
+		?>
+			<div class="container">
+				<?php
+					while ( have_posts() ) : the_post();
+					the_title();
+					the_excerpt();
+					endwhile;
+				else :
+					_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+			?>
+			</div>			
+		<?php endif; ?>
+	</main>
 </div>
-
 <?php
 
 get_footer();
