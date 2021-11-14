13b5492e7f9346f507abfd8788db73481cf59076
Nishant-Kapadiya <nishantkapadiya8@gmail.com>
2021-11-14 11:23:11 +0530

    update

--- index.php
+++ index.php
@@ -9,25 +9,59 @@ get_header();
 
 ?>
 <div id="primary">
-	<main id="main" class="site-main mt-5" role="main">		
-		<?php
-			if ( have_posts() ) : 
-		?>
-			<div class="container">
-				<?php
-					while ( have_posts() ) : the_post();
-					the_title();
-					the_excerpt();
-					endwhile;
+		<main id="main" class="site-main mt-5" role="main">
+			<?php
+			if ( have_posts() ) :
+				?>
+				<div class="container">
+					<?php
+					if ( is_home() && ! is_front_page() ) {
+						?>
+						<header class="mb-5">
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
+						</header>
+						<?php
+					}
+					?>
+
+					<div class="row">
+						<?php
+						$index = 0;
+						$no_of_columns = 3;
+
+						while ( have_posts() ) : the_post();
+
+							if ( 0 === $index % $no_of_columns ) {
+								?>
+								<div class="col-lg-4 col-md-6 col-sm-12">
+								<?php
+							}
+
+							?>
+							<h3><?php the_title(); ?></h3>
+							<p><?php the_content(); ?></p>
+							<?php
+							$index ++;
+							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+								?>
+								</div>
+								<?php
+							}
+
+						endwhile;
+						?>
+					</div>
+				</div>
+			<?php
 				else :
 					_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+
+				endif;
 			?>
-			</div>			
-		<?php endif; ?>
-	</main>
-</div>
+		</main>
+	</div>
 <?php
-
-get_footer();
-
+	get_footer();
 ?>
